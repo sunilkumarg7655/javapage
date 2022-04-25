374b8bc0a65f5dc21dc1b1fcac16a517c6ff7946
sunilkumarg7655 <76213375+sunilkumarg7655@users.noreply.github.com>
2022-04-25 11:05:13 +0530

    Update MainController.java

--- src/main/java/com/baeldung/controllers/MainController.java
+++ src/main/java/com/baeldung/controllers/MainController.java
@@ -9,7 +9,7 @@ public class MainController {
 
     @GetMapping("/")
     public String viewIndexPage(Model model) {
-        model.addAttribute("header", "Welcome");
+        model.addAttribute("header", "javapage");
         return "index";
     }
 }
